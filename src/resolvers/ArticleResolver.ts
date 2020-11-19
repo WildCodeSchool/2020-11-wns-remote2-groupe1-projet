af514f56e02f843eeea3341eada71ac6324b2149
JDucellier <jeanducellier.dev@gmail.com>
2020-11-19 17:47:04 +0100

    :construction: Add Create Delete and Update Methods for Article :construction:

---
+++ src/resolvers/ArticleResolver.ts
@@ -0,0 +1,48 @@
+import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import CreateArticleInput from '../inputs/CreateArticleInput';
+import UpdateArticleInput from '../inputs/UpdateArticleInput';
+import { Article } from '../models/Article';
+
+@Resolver()
+export default class ArticleResolver {
+  @Query(() => [Article])
+  articles(): Promise<Article[]> {
+    return Article.find();
+  }
+
+  @Mutation(() => Article)
+  async createArticle(@Arg('data') data: CreateArticleInput): Promise<Article> {
+    const article = Article.create(data);
+    await article.save();
+    return article;
+  }
+
+  @Mutation(() => Article)
+  async updateArticle(
+    @Arg('id') id: string,
+    @Arg('data') data: UpdateArticleInput
+  ): Promise<Article> {
+    const article = await Article.findOne({ where: { id } });
+
+    if (!article) {
+      throw new Error(`The article with id: ${id} does not exist!`);
+    }
+
+    Object.assign(article, data);
+    await article.save();
+
+    return article;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteArticle(@Arg('id') id: string): Promise<boolean> {
+    const article = await Article.findOne({ where: { id } });
+
+    if (!article) {
+      throw new Error(`The article with id: ${id} does not exist!`);
+    }
+
+    await article.remove();
+    return true;
+  }
+}
