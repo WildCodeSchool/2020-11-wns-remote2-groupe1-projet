5f4705dfebc1da92cae9f877ecaa699aae9d1211
Simon <simon@flitdesk.com>
2020-11-20 08:15:01 +0100

    :construction:comment-creation-done:construction:

--- src/resolvers/CommentResolver.ts
+++ src/resolvers/CommentResolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import CreateCommentInput from '../inputs/CreateCommentInput';
 import { Comment } from '../models/Comment';
 
 @Resolver()
@@ -6,5 +7,12 @@ export default class CommmentResolver {
   @Query(() => [Comment])
   comments(): Promise<Comment[]> {
     return Comment.find();
+  }
+
+  @Mutation(() => Comment)
+  async createComment(@Arg('data') data: CreateCommentInput): Promise<Comment> {
+    const commment = Comment.create(data);
+    await commment.save();
+    return commment;
   }
 }
