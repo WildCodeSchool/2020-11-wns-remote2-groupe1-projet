67e8e8b5068d625c2f2db0bef53332d6f32d74bf
JDucellier <jeanducellier.dev@gmail.com>
2021-01-06 15:06:30 +0100

    :construction: Build template for login:construction:

---
+++ web-client/src/components/layout/Header/SignModal/LogIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LogInComponent = () => {
+  return (
+    <div>
+      <p>LogIn Component</p>
+    </div>
+  );
+};
+export default LogInComponent;
--- web-client/src/components/layout/Header/index.tsx
+++ web-client/src/components/layout/Header/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import ArticleList from '../articles/ArticleList';
-import Home from '../Home';
-import {
-  Button,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import SignModal from './SignModal';
 
 const Header = () => {
   return (
@@ -26,6 +23,7 @@ const Header = () => {
           <Button variant="contained" color="primary">
             <Link to="/articles">Articles</Link>
           </Button>
+          <SignModal />
         </Toolbar>
       </AppBar>
     </header>
