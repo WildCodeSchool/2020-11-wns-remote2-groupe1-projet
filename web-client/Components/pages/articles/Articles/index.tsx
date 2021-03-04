0c93f7fd91b1c0aff04d2a98534c0b46a00d8ae9
daniel <danielo.visage@gmail.com>
2021-03-04 14:19:05 +0100

    :construction: set up ellipsis for article preview text :construction:

--- web-client/Components/pages/articles/Articles/index.tsx
+++ web-client/Components/pages/articles/Articles/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ArticleCard from '../ArticleCard';
 import { makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
 const Articles = (): JSX.Element => {
   const articles = [
     {
-      id: 1,
+      id: '1',
       title: 'First Article',
       image:
         'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YnVyZ2VyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
@@ -30,7 +30,7 @@ const Articles = (): JSX.Element => {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
     {
-      id: 2,
+      id: '2',
       title: 'Second Article',
       image:
         'https://www.techrado.com/wp-content/uploads/2019/10/best-laptops-programming.jpg',
@@ -38,7 +38,7 @@ const Articles = (): JSX.Element => {
         'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
     },
     {
-      id: 3,
+      id: '3',
       title: 'Third Article',
       image:
         'https://tul.imgix.net/content/article/things-to-do-queenstown-7.jpg?auto=format,compress&w=1200&h=630&fit=crop',
@@ -46,7 +46,7 @@ const Articles = (): JSX.Element => {
         'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
     },
     {
-      id: 4,
+      id: '4',
       title: 'Fourth Article',
       image:
         'https://tul.imgix.net/content/article/things-to-do-queenstown-7.jpg?auto=format,compress&w=1200&h=630&fit=crop',
@@ -54,8 +54,24 @@ const Articles = (): JSX.Element => {
         'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
     },
     {
-      id: 5,
+      id: '5',
       title: 'Fifth Article',
+      image:
+        'https://tul.imgix.net/content/article/things-to-do-queenstown-7.jpg?auto=format,compress&w=1200&h=630&fit=crop',
+      contents:
+        'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+    },
+    {
+      id: '6',
+      title: 'Sixth Article',
+      image:
+        'https://tul.imgix.net/content/article/things-to-do-queenstown-7.jpg?auto=format,compress&w=1200&h=630&fit=crop',
+      contents:
+        'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.',
+    },
+    {
+      id: '7',
+      title: 'Seventh Article',
       image:
         'https://tul.imgix.net/content/article/things-to-do-queenstown-7.jpg?auto=format,compress&w=1200&h=630&fit=crop',
       contents:
@@ -69,16 +85,18 @@ const Articles = (): JSX.Element => {
       <Typography variant="h2" className={classes.textCenter}>
         News Feed
       </Typography>
+      <Grid>
+        {articles.map((article) => (
+          <ArticleCard
+            key={article.id}
+            id={article.id}
+            title={article.title}
+            image={article.image}
+            contents={article.contents}
+          />
+        ))}
+      </Grid>
 
-      {articles.map((article) => (
-        <ArticleCard
-          key={article.id}
-          id={article.id}
-          title={article.title}
-          image={article.image}
-          contents={article.contents}
-        />
-      ))}
       <Pagination
         className={classes.pagination}
         count={5}
