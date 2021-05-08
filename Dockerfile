4de773823ab3cf86ff5ea2eb31aa76cda5b5557f
daniel <danielo.visage@gmail.com>
2021-05-08 14:55:02 +0200

    wip: fix connection

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 
 FROM node:15.11-alpine
 
+RUN mkdir -p /usr/src/app
+RUN mkdir -p /usr/src/app/backend
+
 WORKDIR /app
 
 COPY package*.json ./
@@ -8,3 +11,7 @@ COPY package*.json ./
 RUN npm install
 
 COPY . ./
+
+EXPOSE 4000
+
+CMD npm run dev
--- web-client/next.config.js
+++ web-client/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/:path',
-        destination: 'http://localhost:4000/graphql', // Proxy to Backend
+        destination: 'http://[::1]:4000/graphql', // Proxy to Backend
       },
     ];
   },
