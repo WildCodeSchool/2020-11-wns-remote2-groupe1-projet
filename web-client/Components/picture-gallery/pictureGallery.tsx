242a750b35a0dcabb4bf44ddf8cd79a35301866f
Daniel <danielo.visage@gmail.com>
2021-07-04 10:55:28 +0200

    clean up gallery style, put images in cards, stylize upload button, add delete btn to cards (wip: not functional)

--- web-client/Components/picture-gallery/pictureGallery.tsx
+++ web-client/Components/picture-gallery/pictureGallery.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Button } from '@material-ui/core';
+import {
+  Typography,
+  Button,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Avatar,
+  Grid,
+} from '@material-ui/core';
 import { gql, useMutation, useQuery } from '@apollo/client';
 import { GetPictures, UploadPicture } from '../../src/schemaTypes';
 
@@ -10,23 +19,46 @@ const useStyles = makeStyles((theme) => ({
       marginTop: theme.spacing(2),
     },
   },
-  image: {
-    width: '100%',
+  imageCard: {
     margin: '1rem',
+  },
+  image: {
+    maxWidth: '100%',
+    maxHeight: '100%',
+    [theme.breakpoints.up('md')]: {
+      maxWidth: '23rem',
+      maxHeight: '23rem',
+    },
+  },
+  uploadButton: {
+    cursor: 'pointer',
+    display: 'block',
+    margin: '2rem auto 2rem auto',
+    width: '10rem',
+    textAlign: 'center',
   },
   gallery: {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
+    margin: 'auto',
+    width: '90vw',
   },
-  feedTitle: {
+  galleryTitle: {
     textAlign: 'center',
     fontWeight: 'bold',
     color: '#1b84c1',
   },
-  feedButton: {
-    display: 'block',
-    margin: '2rem auto 2rem auto',
+  cardContent: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  deleteBtn: {
+    backgroundColor: '#ffa4a2',
+    width: '10rem',
+    justifyContent: 'center',
+    margin: 'auto',
+    marginTop: '1rem',
   },
 }));
 
@@ -83,15 +115,23 @@ const PictureGalleryComponent = (): JSX.Element => {
   };
   return (
     <>
-      <h1>Picture Gallery</h1>
+      <h1 className={classes.galleryTitle}>Picture Gallery</h1>
       <input
         type="file"
         id="file"
         required
         accept="image/*"
+        hidden
         onChange={uploadPicture}
       />
-      <label htmlFor="file">Add a picture</label>
+      <Button
+        className={classes.uploadButton}
+        color="primary"
+        variant="contained"
+        size="small"
+      >
+        <label htmlFor="file">Add a picture</label>
+      </Button>
 
       <div className={classes.gallery}>
         {loading ? (
@@ -101,10 +141,22 @@ const PictureGalleryComponent = (): JSX.Element => {
         ) : (
           data?.pictures.map(({ id, extension }) => (
             <div key={id}>
-              <img
-                className={classes.image}
-                src={`http://localhost:4000/public/media/pictures/${id}${extension}`}
-              />
+              <Card className={classes.imageCard}>
+                <CardContent className={classes.cardContent}>
+                  <img
+                    className={classes.image}
+                    src={`http://localhost:4000/public/media/pictures/${id}${extension}`}
+                  />
+                  <Button
+                    className={classes.deleteBtn}
+                    color="primary"
+                    variant="contained"
+                    size="small"
+                  >
+                    Delete
+                  </Button>
+                </CardContent>
+              </Card>
             </div>
           ))
         )}
