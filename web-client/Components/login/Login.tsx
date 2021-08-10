d08d9e85f747cedd3142ffcbd407dd1bd4d24340
Daniel <danielo.visage@gmail.com>
2021-08-10 12:44:19 +0200

    replace components with login component when not logged in

---
+++ web-client/Components/login/Login.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import { NextPage } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import Link from 'next/link';
+import { LOGIN_MUTATION } from '../../src/queries';
+
+const useStyles = makeStyles({
+  root: {
+    padding: 32,
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: 64,
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
+
+const LoginComponent = () => {
+  const router = useRouter();
+  const classes = useStyles();
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [login, { error }] = useMutation(LOGIN_MUTATION, {
+    onCompleted() {
+      router.push('/');
+    },
+  });
+
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h2">Login Page</Typography>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const {} = await login({
+            variables: { email, password },
+          });
+        }}
+      >
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error={!!error}
+              fullWidth
+              type="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              error={!!error}
+              fullWidth
+              type="password"
+            />
+          </Grid>
+          <Grid item xs={12} className={classes.buttons}>
+            <Link href="/register">
+              <Button>Register</Button>
+            </Link>
+            <Button variant="contained" color="primary" type="submit">
+              Sign In
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
+  );
+};
+export default LoginComponent;
--- web-client/pages/login/index.tsx
+++ web-client/pages/login/index.tsx
@@ -1,86 +1,12 @@
-import React, { useState, useContext } from 'react';
-import { useMutation } from '@apollo/client';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/styles';
-import Grid from '@material-ui/core/Grid';
+import React from 'react';
 import { NextPage } from 'next';
-import { useRouter } from 'next/dist/client/router';
-import Link from 'next/link';
-import { UserContext } from '../../contexts/Contexts';
-import { LOGIN_MUTATION } from '../../src/queries';
-
-const useStyles = makeStyles({
-  root: {
-    padding: 32,
-    maxWidth: 600,
-    margin: 'auto',
-    marginTop: 64,
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-});
+import LoginComponent from '../../components/login/Login';
 
 const Login: NextPage = () => {
-  const { me } = useContext(UserContext);
-  const router = useRouter();
-  const classes = useStyles();
-
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [login, { error }] = useMutation(LOGIN_MUTATION, {
-    onCompleted() {
-      router.push('/');
-    },
-  });
-
   return (
-    <Paper className={classes.root}>
-      <Typography variant="h2">Login Page</Typography>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          const {} = await login({
-            variables: { email, password },
-          });
-        }}
-      >
-        <Grid container justify="center" spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              label="Adresse email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              error={!!error}
-              fullWidth
-              type="email"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Mot de passe"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              error={!!error}
-              fullWidth
-              type="password"
-            />
-          </Grid>
-          <Grid item xs={12} className={classes.buttons}>
-            <Link href="/register">
-              <Button>Créer un compte</Button>
-            </Link>
-            <Button variant="contained" color="primary" type="submit">
-              Connexion
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </Paper>
+    <div>
+      <LoginComponent />
+    </div>
   );
 };
 export default Login;
