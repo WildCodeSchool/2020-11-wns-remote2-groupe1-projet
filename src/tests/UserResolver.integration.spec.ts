795b75b57406b2d1b128302dd78036e17f89e3d2
Daniel <danielo.visage@gmail.com>
2021-09-03 21:18:45 +0200

    cleanup

--- src/tests/UserResolver.integration.spec.ts
+++ src/tests/UserResolver.integration.spec.ts
@@ -76,7 +76,7 @@ describe('User resolvers', () => {
     });
   });
 
-  describe('query me', () => {
+  describe('query currentUser', () => {
     let user: User;
 
     beforeEach(async () => {
@@ -93,7 +93,7 @@ describe('User resolvers', () => {
       it('returns error', async () => {
         const response = await testClient.post('/graphql').send({
           query: `{
-            me {
+            currentUser {
               email
             }
           }
@@ -104,142 +104,141 @@ describe('User resolvers', () => {
       });
     });
 
-    // describe('when user is authenticated', () => {
-    //   it('returns user', async () => {
-    //     const userSession = UserSession.create({ user: User });
-    //     await userSession.save();
+    describe('when user is authenticated', () => {
+      it('returns user', async () => {
+        const userSession = UserSession.create({ user: User });
+        await userSession.save();
 
-    //     const response = await testClient
-    //       .post('/graphql')
-    //       .set('Cookie', [`sessionId=${userSession.uuid}`])
-    //       .send({
-    //         query: `{
-    //             me {
-    //               email
-    //             }
-    //           }
-    //         `,
-    //       });
+        const response = await testClient
+          .post('/graphql')
+          .set('Cookie', [`sessionId=${userSession.uuid}`])
+          .send({
+            query: `{
+                currentUser {
+                  email
+                }
+              }
+            `,
+          });
 
-    //     expect(JSON.parse(response.text).data).toEqual({
-    //       me: {
-    //         email: user.email,
-    //       },
-    //     });
-    //   });
-    // });
+        expect(JSON.parse(response.text).data).toEqual({
+          currentUser: {
+            email: user.email,
+          },
+        });
+      });
+    });
 
-    // describe('mutation createUser', () => {
-    //   it('creates and returns a new user', async () => {
-    //     const response = await testClient.post('/graphql').send({
-    //       query: `mutation {
-    //         createUser(
-    //           input: {
-    //             email: "lucrenaud@gmail.com"
-    //             password: "bateaubateau"
-    //             firstName: "Arman"
-    //             lastName: "Durand"
-    //             birthDate: '10/10/1980',
-    //             school: 'Ecole de Rennes',
-    //             role: UserRole.STUDENT,
-    //           }
-    //         ) {
-    //           firstName
-    //           lastName
-    //         }
-    //       }
-    //       `,
-    //     });
+    describe('mutation createUser', () => {
+      it('creates and returns a new user', async () => {
+        const response = await testClient.post('/graphql').send({
+          query: `mutation {
+            createUser(
+              input: {
+                email: "lucrenaud@gmail.com"
+                password: "bateaubateau"
+                firstName: "Arman"
+                lastName: "Durand"
+                birthDate: '10/10/1980',
+              }
+            ) {
+              firstName
+              lastName
+            }
+          }
+          `,
+        });
 
-    //     expect(await User.count({})).toEqual(1);
-    //     expect(JSON.parse(response.text).data).toEqual({
-    //       createUser: {
-    //         firstName: 'Arman',
-    //         lastName: 'Durand',
-    //       },
-    //     });
-    //   });
-    // });
+        expect(await User.count({})).toEqual(1);
+        expect(JSON.parse(response.text).data).toEqual({
+          createUser: {
+            firstName: 'Arman',
+            lastName: 'Durand',
+          },
+        });
+      });
+    });
 
-    // describe('mutation createSession', () => {
-    //   describe('when username does not match existing user', () => {
-    //     it('returns error', async () => {
-    //       const response = await testClient.post('/graphql').send({
-    //         query: `mutation {
-    //             createSession(input: {
-    //               email: "vmarchand@gmail.com",
-    //               password: "bateau8888"
-    //             }) {
-    //               email
-    //             }
-    //           }
-    //           `,
-    //       });
-    //       expect(response.text).toMatch('Incorrect username and/or password.');
-    //     });
-    //   });
+    describe('mutation createSession', () => {
+      describe('when username does not match existing user', () => {
+        it('returns error', async () => {
+          const response = await testClient.post('/graphql').send({
+            query: `mutation {
+                createSession(input: {
+                  email: "vmarchand@gmail.com",
+                  password: "bateau8888"
+                }) {
+                  email
+                }
+              }
+              `,
+          });
+          expect(response.text).toMatch('Incorrect username and/or password.');
+        });
+      });
 
-    //   describe('when username matches existing user', () => {
-    //     let user: User;
-    //     beforeEach(async () => {
-    //       user = User.create({
-    //         email: 'laurepincon@gmail.com',
-    //         password: 'laurepassword',
-    //         firstName: 'Laure',
-    //         lastName: 'Pinçon',
-    //         birthDate: '10/10/1980',
-    //         school: 'Ecole de Reims',
-    //         role: UserRole.STUDENT,
-    //       });
-    //       await user.save();
-    //     });
+      describe('when username matches existing user', () => {
+        let user: User;
+        beforeEach(async () => {
+          user = User.create({
+            email: 'laurepincon@gmail.com',
+            password: 'laurepassword',
+            firstName: 'Laure',
+            lastName: 'Pinçon',
+            birthDate: '10/10/1980',
+          });
+          await user.save();
+        });
 
-    //     describe('when password does not match user password', () => {
-    //       it('returns error', async () => {
-    //         const response = await testClient.post('/graphql').send({
-    //           query: `mutation {
-    //             createSession(input: {
-    //               email: "laurepincon@gmail.com",
-    //               password: "lauremotdepasse"
-    //             }) {
-    //               email
-    //             }
-    //           }
-    //           `,
-    //         });
-    //         expect(response.text).toMatch('Incorrect username and/or password.');
-    //       });
-    //     });
+        describe('when password does not match user password', () => {
+          it('returns error', async () => {
+            const response = await testClient.post('/graphql').send({
+              query: `mutation {
+                createSession(input: {
+                  email: "laurepincon@gmail.com",
+                  password: "lauremotdepasse"
+                }) {
+                  email
+                }
+              }
+              `,
+            });
+            expect(response.text).toMatch(
+              'Incorrect username and/or password.'
+            );
+          });
+        });
 
-    //     describe('when password matches user password', () => {
-    //       it('creates user session and sets cookie with user session ID', async () => {
-    //         const response = await testClient.post('/graphql').send({
-    //           query: `mutation {
-    //             createSession(input: {
-    //               email: "laurepincon@gmail.com",
-    //               password: "laurepassword"
-    //             }) {
-    //               email
-    //             }
-    //           }
-    //           `,
-    //         });
+        describe('when password matches user password', () => {
+          it('creates user session and sets cookie with user session ID', async () => {
+            const response = await testClient.post('/graphql').send({
+              query: `mutation {
+                createSession(input: {
+                  email: "laurepincon@gmail.com",
+                  password: "laurepassword"
+                }) {
+                  email
+                }
+              }
+              `,
+            });
 
-    //         expect(JSON.parse(response.text).data.createSession).toEqual({
-    //           email: 'laurepincon@gmail.com',
-    //         });
+            expect(JSON.parse(response.text).data.createSession).toEqual({
+              email: 'laurepincon@gmail.com',
+            });
 
-    //         const userSession = await UserSession.findOneOrFail({
-    //           user: User,
-    //         });
-    //         const cookie = response.header['set-cookie'][0];
-    //         expect(cookie).toMatch(`sessionId=${userSession.uuid}`);
-    //         expect(cookie).toMatch(`Path=/`);
-    //         expect(cookie).toMatch(`Max-Age=2592000`);
-    //         expect(cookie).toMatch(`HttpOnly`);
-    //         expect(cookie).toMatch(`SameSite=Strict`);
-    //       });
-    //     });
-    //   });
+            const userSession = await UserSession.findOneOrFail({
+              user: User,
+            });
+            const cookie = response.header['set-cookie'][0];
+            expect(cookie).toMatch(`sessionId=${userSession.uuid}`);
+            expect(cookie).toMatch(`Path=/`);
+            expect(cookie).toMatch(`Max-Age=2592000`);
+            expect(cookie).toMatch(`HttpOnly`);
+            expect(cookie).toMatch(`SameSite=Strict`);
+          });
+        });
+      });
+    });
   });
 });
